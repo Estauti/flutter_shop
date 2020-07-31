cef4efcad99511776f05ef7a21007615b22120f6
Estauti <estautib@gmail.com>
2020-07-30 22:15:31 -0300

    Add provider package and products provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/products_provider.dart';
 
 import './screens/products_overview_screen.dart';
 
@@ -9,14 +11,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (context) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/product_item.dart';
+
+import '../providers/products_provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final products = productsProvider.products;
+
+    return GridView.builder(
+      padding: EdgeInsets.all(15),
+      itemCount: products.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      itemBuilder: (context, index) => ProductItem(
+        id: products[index].id,
+        title: products[index].title,
+        imageUrl: products[index].imageUrl,
+      ),
+    );
+  }
+}
